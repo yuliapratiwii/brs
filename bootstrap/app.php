f661a28c94698ee4fefc5f7c82577678ebc07227
Yulia Pratiwi <99121563+yuliapratiwii@users.noreply.github.com>
2026-08-29 22:20:49 +0700

    y1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,7 @@ $app = Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
